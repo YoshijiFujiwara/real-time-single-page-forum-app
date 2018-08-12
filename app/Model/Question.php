18044519ba25a5c5e17a7ec668440c47908addba
vagrant <vagrant@homestead>
2018-08-12 00:43:46 +0000

    CRUD for Question along with Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,9 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    /**
+     * /api/question/[スラグ名]
+     * で、データを取得できるようにする
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = []; // $fillable　より楽
+
     public function user()
     {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function replies()
@@ -20,5 +32,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * +--------+-----------+-------------------------+------------------+-------------------------------------------------+------------+
+ * | Domain | Method    | URI                     | Name             | Action                                          | Middleware |
+ * +--------+-----------+-------------------------+------------------+-------------------------------------------------+------------+
+ * |        | GET|HEAD  | /                       |                  | Closure                                         | web        |
+ * |        | GET|HEAD  | api/question            | question.index   | App\Http\Controllers\QuestionController@index   | api        |
+ * |        | POST      | api/question            | question.store   | App\Http\Controllers\QuestionController@store   | api        |
+ * |        | GET|HEAD  | api/question/{question} | question.show    | App\Http\Controllers\QuestionController@show    | api        |
+ * |        | PUT|PATCH | api/question/{question} | question.update  | App\Http\Controllers\QuestionController@update  | api        |
+ * |        | DELETE    | api/question/{question} | question.destroy | App\Http\Controllers\QuestionController@destroy | api        |
+ * +--------+-----------+-------------------------+------------------+-------------------------------------------------+------------+
+ */
+Route::apiResource('/question', 'QuestionController');
