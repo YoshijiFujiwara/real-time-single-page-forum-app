1990e8e72ed0839f6ea9141e8324a29fcf6d82fb
vagrant <vagrant@homestead>
2018-08-12 01:42:11 +0000

    CRUD for Category

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,5 @@
  * +--------+-----------+-------------------------+------------------+-------------------------------------------------+------------+
  */
 Route::apiResource('/question', 'QuestionController');
+
+Route::apiResource('/category', 'CategoryController');
